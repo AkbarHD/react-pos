605d9eaf1742302634b47860aee7943e7d9e9583
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2025-06-19 15:57:28 +0700

    relasi model

--- app/Models/StockOpnameDetail.php
+++ app/Models/StockOpnameDetail.php
@@ -1,10 +1,22 @@
 <?php
 
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class StockOpnameDetail extends Model
 {
-     protected $guarded = [];
+    protected $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function stockTotal()
+    {
+        return $this->belongsTo(StockTotal::class, 'product_id', 'product_id');
+    }
+
 }
